c0e8f42647ac90e4765873b7b485950508339afd
speedy <thenewamirech@gmail.com>
2025-08-24 06:38:01 +0330

    Added StudentController

---
+++ src/main/java/ir/speedy/studentservice/controllers/StudentController.java
@@ -0,0 +1,34 @@
+package ir.speedy.studentservice.controllers;
+
+import ir.speedy.studentservice.models.Student;
+import ir.speedy.studentservice.services.StudentService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/student/v1")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping("/show")
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+
+    @GetMapping("/{id}")
+    public Student GetStudentById(@PathVariable int id) {
+        return studentService.getStudentById(id);
+    }
+
+    @PostMapping("/insert")
+    public Student InsertStudent(@RequestBody Student student) {
+        return studentService.insertStudent(student);
+    }
+
+}
