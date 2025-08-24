764373d2a2ddba9ddce75a001e1f7e43164a4b7e
speedy <thenewamirech@gmail.com>
2025-08-24 06:36:30 +0330

    Added StudentRepository

---
+++ src/main/java/ir/speedy/studentservice/repositories/StudentRepository.java
@@ -0,0 +1,9 @@
+package ir.speedy.studentservice.repositories;
+
+import ir.speedy.studentservice.models.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+}
