14c8b1447a0aca65480d55ddf9df67acac124a51
speedy <thenewamirech@gmail.com>
2025-08-24 08:50:53 +0330

    Mapped getLibrary api into getStudent api

--- src/main/java/ir/speedy/studentservice/controllers/StudentController.java
+++ src/main/java/ir/speedy/studentservice/controllers/StudentController.java
@@ -1,19 +1,22 @@
 package ir.speedy.studentservice.controllers;
 
+import ir.speedy.studentservice.models.Library;
 import ir.speedy.studentservice.models.Student;
 import ir.speedy.studentservice.services.StudentService;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/student/v1")
 public class StudentController {
-
+    private final RestTemplate restTemplate;
     private final StudentService studentService;
 
-    public StudentController(StudentService studentService) {
+    public StudentController(StudentService studentService, RestTemplate restTemplate) {
         this.studentService = studentService;
+        this.restTemplate = restTemplate;
     }
 
     @GetMapping("/show")
@@ -22,12 +25,15 @@ public class StudentController {
     }
 
     @GetMapping("/{id}")
-    public Student GetStudentById(@PathVariable int id) {
-        return studentService.getStudentById(id);
+    public Student getStudentById(@PathVariable int id) {
+        Library library = restTemplate.getForObject("http://localhost:9092/api/library/v1/" + id, Library.class);
+        Student student =  studentService.getStudentById(id);
+        student.setLibrary(library);
+        return student;
     }
 
     @PostMapping("/insert")
-    public Student InsertStudent(@RequestBody Student student) {
+    public Student insertStudent(@RequestBody Student student) {
         return studentService.insertStudent(student);
     }
 
