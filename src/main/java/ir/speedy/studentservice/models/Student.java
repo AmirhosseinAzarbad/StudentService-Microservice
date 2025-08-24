ddf172cf4572840e7cd70a3b4b3e0e6bd63b1d45
speedy <thenewamirech@gmail.com>
2025-08-24 06:26:50 +0330

    Added Student entity

---
+++ src/main/java/ir/speedy/studentservice/models/Student.java
@@ -0,0 +1,21 @@
+package ir.speedy.studentservice.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "studentTable")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String fatherName;
+    private String address;
+    private Integer age;
+}
